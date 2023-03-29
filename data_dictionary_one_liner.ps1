806568bc51052f6ab7f7968bfe32ea7db19bff02
Matthew E. Walls <78390378+m-e-w@users.noreply.github.com>
2023-03-29 13:27:39 -0400

    Removed desciption column. Updated column order.

--- data_dictionary_one_liner.ps1
+++ data_dictionary_one_liner.ps1
@@ -1,4 +1,4 @@
 $username = 'admin'; 
 $password = 'adm!nd42'; 
 $instance = '192.168.1.0'; 
-((curl.exe -k -u "$($username):$($password)" "https://$($instance)/services/data/v1.0/dd/") | ConvertFrom-Json) | Select-Object -Property view -ExpandProperty columns | Export-CSV dd.csv -NoTypeInformation
+((curl.exe -k -u "$($username):$($password)" "https://$($instance)/services/data/v1.0/dd/") | ConvertFrom-Json) | Select-Object -Property view -ExpandProperty columns | Select-Object view, column, data_type, description | Export-CSV dd.csv -NoTypeInformation
